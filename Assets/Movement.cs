fb7b51763c6f3497e8524552c02e6639371dc5dc
Fisac <isac_degerborg@hotmail.com>
2018-07-12 17:17:23 +0200

    Added player and level

---
+++ Assets/Movement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	[SerializeField]
+	private float movementSpeed, jumpForce;
+
+	private bool canJump;
+
+	private Rigidbody2D rb;
+
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void Update ()
+	{
+		ReadInputs();
+	}
+
+	private void ReadInputs()
+	{
+		DirectionalInput();
+		JumpInput();
+	}
+
+	private void DirectionalInput()
+	{
+		rb.velocity = new Vector2(Input.GetAxis("Horizontal") * movementSpeed, rb.velocity.y);
+	}
+
+	private void JumpInput()
+	{
+		if (Input.GetButtonDown("Jump") && canJump)
+		{
+			rb.AddForce(Vector2.up * jumpForce);
+		}
+	}
+}
