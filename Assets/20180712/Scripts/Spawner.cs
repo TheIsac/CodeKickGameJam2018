ce8a709ea7f18dae5a2de83e0694b532616e55d2
David Jensen <davv.js@gmail.com>
2018-07-12 17:07:56 +0200

    Spawner

---
+++ Assets/20180712/Scripts/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Spawner : MonoBehaviour
+{
+	public int MinSpawnX = 0;
+	public int MaxSpawnX = 20;
+	public int MinSpawnY = 0;
+	public int MaxSpawnY = 20;
+	public float SecondsBetweenSpawn = 1;
+	public GameObject Prefab;
+
+	private float secondsSinceLastSpawn = 0;
+
+	void Update()
+	{
+		secondsSinceLastSpawn += Time.deltaTime;
+		if (secondsSinceLastSpawn >= SecondsBetweenSpawn)
+		{
+			secondsSinceLastSpawn = 0;
+			var xPos = Random.Range(MinSpawnX, MaxSpawnX);
+			var yPos = Random.Range(MinSpawnY, MaxSpawnY);
+			Instantiate(Prefab,  new Vector2(xPos ,yPos), Quaternion.identity);
+		}
+	}
+}
