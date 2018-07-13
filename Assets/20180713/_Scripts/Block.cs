36a2261ed9db8a5464f159097b3ff18b4ebad153
augustalex <augustalexandersson@gmail.com>
2018-07-13 17:19:37 +0200

    Started work on Blocks

---
+++ Assets/20180713/_Scripts/Block.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    private bool isFree = false;
+
+    public bool IsFree()
+    {
+        return isFree;
+    }
+
+    public void Hold()
+    {
+        isFree = false;
+    }
+}
---
+++ Assets/20180713/_Scripts/BlockHolder.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace _20180713._Scripts
+{
+    public class BlockHolder : MonoBehaviour
+    {
+        private Block holdingBlock;
+
+        private readonly Stack<Block> touchingBlocks = new Stack<Block>();
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (touchingBlocks.Count > 0 && holdingBlock == null)
+            {
+                holdingBlock = touchingBlocks.Pop();
+                if (holdingBlock.IsFree())
+                {
+                    holdingBlock.Hold();
+                }
+            }
+        }
+
+        public bool IsHoldingBlock()
+        {
+            return holdingBlock != null;
+        }
+
+        public Block GetHoldingBlock()
+        {
+            return holdingBlock;
+        }
+
+        public Block ReleaseHoldingBlock()
+        {
+            var block = holdingBlock;
+            holdingBlock = null;
+            return block;
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            var block = other.collider.GetComponent<Block>();
+            if (block)
+            {
+                touchingBlocks.Push(block);
+            }
+        }
+
+        private void OnCollisionExit(Collision other)
+        {
+            var block = other.collider.GetComponent<Block>();
+            if (block)
+            {
+                touchingBlocks.Push(block);
+            }
+        }
+    }
+}
