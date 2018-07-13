3782bf94f20c74ba89d5a7f59817aa379036811e
augustalex <augustalexandersson@gmail.com>
2018-07-13 18:13:00 +0200

    Player can pickup blocks

--- Assets/20180713/_Scripts/Block.cs
+++ Assets/20180713/_Scripts/Block.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using _20180713._Scripts;
 
 public class Block : MonoBehaviour
 {
-    private bool isFree = false;
+    private bool isFree = true;
 
     public bool IsFree()
     {
@@ -14,5 +15,19 @@ public class Block : MonoBehaviour
     public void Hold()
     {
         isFree = false;
+    }
+
+    public void Release()
+    {
+        isFree = true;
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        var blockHolder = collider.GetComponent<BlockHolder>();
+        if (isFree && blockHolder && !blockHolder.IsHoldingBlock())
+        {
+            blockHolder.SetHoldingBlock(this);
+        }
     }
 }
--- Assets/20180713/_Scripts/BlockHolder.cs
+++ Assets/20180713/_Scripts/BlockHolder.cs
@@ -5,26 +5,21 @@ namespace _20180713._Scripts
 {
     public class BlockHolder : MonoBehaviour
     {
+        public Transform HoldingPoint;
+
         private Block holdingBlock;
-
-        private readonly Stack<Block> touchingBlocks = new Stack<Block>();
-
-        // Update is called once per frame
-        void Update()
-        {
-            if (touchingBlocks.Count > 0 && holdingBlock == null)
-            {
-                holdingBlock = touchingBlocks.Pop();
-                if (holdingBlock.IsFree())
-                {
-                    holdingBlock.Hold();
-                }
-            }
-        }
 
         public bool IsHoldingBlock()
         {
             return holdingBlock != null;
+        }
+
+        public void SetHoldingBlock(Block block)
+        {
+            holdingBlock = block;
+            holdingBlock.transform.SetParent(transform);
+            block.Hold();
+            block.transform.position = HoldingPoint.position;
         }
 
         public Block GetHoldingBlock()
@@ -36,25 +31,8 @@ namespace _20180713._Scripts
         {
             var block = holdingBlock;
             holdingBlock = null;
+            block.transform.SetParent(null);
             return block;
-        }
-
-        private void OnCollisionEnter(Collision other)
-        {
-            var block = other.collider.GetComponent<Block>();
-            if (block)
-            {
-                touchingBlocks.Push(block);
-            }
-        }
-
-        private void OnCollisionExit(Collision other)
-        {
-            var block = other.collider.GetComponent<Block>();
-            if (block)
-            {
-                touchingBlocks.Push(block);
-            }
         }
     }
 }
