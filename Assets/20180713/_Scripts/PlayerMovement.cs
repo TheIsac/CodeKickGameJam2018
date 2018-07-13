dde3c296e2b86abb682e2e94fa9fe73a16878645
Fisac <isac_degerborg@hotmail.com>
2018-07-13 16:59:03 +0200

    Added movement script

---
+++ Assets/20180713/_Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+
+	[SerializeField]
+	private float movementSpeed;
+	private bool canJump;
+
+	[SerializeField]
+	private string horizontalInput, verticalInput, interactInput;
+
+	private Rigidbody rb;
+	private BoxCollider boxCollider;
+
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+		boxCollider = GetComponent<BoxCollider>();
+	}
+
+	void Update ()
+	{
+		ReadInputs();
+	}
+
+	#region inputs
+	private void ReadInputs()
+	{
+		DirectionalInput();
+		ButtonInput();
+	}
+
+	private void DirectionalInput()
+	{
+		rb.AddForce(new Vector2(Input.GetAxis(horizontalInput) * movementSpeed * Time.deltaTime, Input.GetAxis(verticalInput) * movementSpeed * Time.deltaTime));
+	}
+
+	private void ButtonInput()
+	{
+		if (Input.GetButton(interactInput))
+		{
+			//EXECUTE INTERACTION
+		}
+	}
+	#endregion
+
+
+}
