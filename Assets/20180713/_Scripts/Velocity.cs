dac49d6e16e11ec09b61d933114ae594451aabbb
David Jensen <davv.js@gmail.com>
2018-07-14 10:16:04 +0200

    Bättre moln

--- Assets/20180713/_Scripts/Velocity.cs
+++ Assets/20180713/_Scripts/Velocity.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class Velocity : MonoBehaviour
 {
 
-	public Vector3 Direction;
-	public float Speed;
-	
-	// Update is called once per frame
-	void Update () {
-		transform.Translate(Direction * Speed);
-	}
+    public Vector3 Direction;
+    public float MinSpeed = 0;
+    public float MaxSpeed = 0;
+
+    // Update is called once per frame
+    void Update() {
+        transform.Translate(Direction * Random.Range(MinSpeed, MaxSpeed));
+    }
 }
