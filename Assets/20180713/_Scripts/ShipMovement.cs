97671fa3b2b5fedf29ac184a502a729c15b7695f
Fisac <isac_degerborg@hotmail.com>
2018-07-14 21:05:55 +0200

    Updated shipmovement

--- Assets/20180713/_Scripts/ShipMovement.cs
+++ Assets/20180713/_Scripts/ShipMovement.cs
@@ -10,71 +10,72 @@ public class ShipMovement : MonoBehaviour
 	public string HorizontalInput, VerticalInput, InteractInput, SecondaryInput;
 	public bool isMounted = false;
 
-    [SerializeField] public float movementSpeed;
+	[SerializeField] public float movementSpeed;
 	private Rigidbody rb;
 	private BoxCollider boxCollider;
 
-    private void Awake()
-    {
-        rb = GetComponentInParent<Rigidbody>();
-        boxCollider = GetComponent<BoxCollider>();
-    }
+	private void Awake()
+	{
+		rb = GetComponentInParent<Rigidbody>();
+		boxCollider = GetComponent<BoxCollider>();
+	}
 
-    void Update()
-    {
-        if (!isMounted)
-            return;
+	void Update()
+	{
+		if (!isMounted)
+			return;
 
-        ReadInputs();
-        AdjustThrusterBlocks();
-    }
+		ReadInputs();
+		AdjustThrusterBlocks();
+	}
 
-    #region inputs
+	#region inputs
 
-    private void ReadInputs()
-    {
-        DirectionalInput();
-    }
+	private void ReadInputs()
+	{
+		DirectionalInput();
+	}
 
-    private void DirectionalInput()
-    {
-        var verticalInput = Input.GetAxis(VerticalInput);
-        var horizontalInput = Input.GetAxis(HorizontalInput);
-        rb.AddForce(new Vector3(horizontalInput * movementSpeed * Time.deltaTime, 0,
-            verticalInput * movementSpeed * Time.deltaTime));
+	private void DirectionalInput()
+	{
+		var verticalInput = Input.GetAxis(VerticalInput);
+		var horizontalInput = Input.GetAxis(HorizontalInput);
+		rb.AddForce(new Vector3(horizontalInput * movementSpeed * Time.deltaTime, 0,
+			verticalInput * movementSpeed * Time.deltaTime));
 
-        if (Mathf.Abs(verticalInput) > 0.5 || Mathf.Abs(horizontalInput) > 0.5)
-        {
-            var currentAngle = transform.rotation.eulerAngles.y;
-            var targetAngle = Mathf.Atan2(horizontalInput, verticalInput) * Mathf.Rad2Deg - 180;
-            var inputAngle = Mathf.DeltaAngle(currentAngle, targetAngle);
-            rb.AddTorque(transform.up * inputAngle * 0.01f);
-        }
+		if (Mathf.Abs(verticalInput) > 0.5 || Mathf.Abs(horizontalInput) > 0.5)
+		{
+			var currentAngle = transform.rotation.eulerAngles.y;
+			var targetAngle = Mathf.Atan2(horizontalInput, verticalInput) * Mathf.Rad2Deg - 180;
+			var inputAngle = Mathf.DeltaAngle(currentAngle, targetAngle);
+			rb.AddTorque(transform.up * inputAngle * 0.01f);
+		}
 
-        if (Input.GetButtonDown(SecondaryInput))
-        {
-        }
-    }
+		if (Input.GetButtonDown(SecondaryInput))
+		{
+		}
+	}
 
-    #endregion
+	#endregion
 
-    public void AdjustThrusterBlocks()
-    {
-        var playerMovementComponent = GetComponent<PilotBlockController>().Owner.GetComponent<PlayerMovement>();
-        var moveDirection = Vector3.zero;
-        moveDirection += Vector3.left * Input.GetAxis(playerMovementComponent.VerticalInput);
-        moveDirection += Vector3.forward * Input.GetAxis(playerMovementComponent.HorizontalInput);
-        moveDirection.y = 0;
-        var baseComponent = GetComponentInParent<Base>();
-        var thrusterBlocks = baseComponent.GetBlocks()
-            .Where(b => b.GetComponentInChildren<ThrusterBlock>())
-            .Select(b => b.GetComponentInChildren<ThrusterBlock>());
-        foreach (var thrusterBlock in thrusterBlocks)
-        {
-            var nozzleController = thrusterBlock.GetComponentInChildren<NozzleController>();
-            if (moveDirection != Vector3.zero)
-            {
-                nozzleController.transform.rotation = Quaternion.LookRotation(moveDirection);
-            }
-        }
-    }
+	public void AdjustThrusterBlocks()
+	{
+		var playerMovementComponent = GetComponent<PilotBlockController>().Owner.GetComponent<PlayerMovement>();
+		var moveDirection = Vector3.zero;
+		moveDirection += Vector3.left * Input.GetAxis(playerMovementComponent.VerticalInput);
+		moveDirection += Vector3.forward * Input.GetAxis(playerMovementComponent.HorizontalInput);
+		moveDirection.y = 0;
+		var baseComponent = GetComponentInParent<Base>();
+		var thrusterBlocks = baseComponent.GetBlocks()
+			.Where(b => b.GetComponentInChildren<ThrusterBlock>())
+			.Select(b => b.GetComponentInChildren<ThrusterBlock>());
+		foreach (var thrusterBlock in thrusterBlocks)
+		{
+			var nozzleController = thrusterBlock.GetComponentInChildren<NozzleController>();
+			if (moveDirection != Vector3.zero)
+			{
+				nozzleController.transform.rotation = Quaternion.LookRotation(moveDirection);
+			}
+		}
+	}
+}
