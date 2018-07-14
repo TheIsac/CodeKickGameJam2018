02fa437491c2507d2083e7995b47caea5fe92ad0
David Jensen <davv.js@gmail.com>
2018-07-14 19:58:52 +0200

    ...

---
+++ Assets/20180713/_Scripts/Explodable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explodable : MonoBehaviour
+{
+    public float ExplodeInSeconds = 10;
+    public float Radius;
+    public float Force;
+    [SerializeField] private float time;
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time > ExplodeInSeconds)
+        {
+            var colldiers = Physics.OverlapSphere(transform.position, Radius);
+            foreach (var collider in colldiers)
+            {
+                var body = collider.GetComponent<Rigidbody>();
+                if (body != null)
+                {
+                    body.AddExplosionForce(Force, transform.position, Radius);   
+                }
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
