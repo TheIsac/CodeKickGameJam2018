9f78d54076536344402a65135e82efcdceac2b3f
David Jensen <davv.js@gmail.com>
2018-07-14 11:04:43 +0200

    Added missing files

---
+++ Assets/20180713/_Scripts/DestroyOnOutside.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Axis { x, y, z }
+public enum Comparison { lessThan, greaterThan }
+
+public class DestroyOnOutside : MonoBehaviour
+{
+
+    public Axis Axis;
+    public Comparison Comparison;
+    public float Value;
+
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Axis == Axis.y) {
+            if (Comparison == Comparison.greaterThan && transform.position.y > Value) {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
