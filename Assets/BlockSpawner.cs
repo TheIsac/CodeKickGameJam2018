47d9afe30c511e29ec48029ca67de388a0f0e6ee
augustalex <augustalexandersson@gmail.com>
2018-07-15 19:08:23 +0200

    Added BlockSpawner prefab

--- Assets/BlockSpawner.cs
+++ Assets/BlockSpawner.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class BlockSpawner : MonoBehaviour
 {
     public List<Block> Prefabs;
 
     public float SecondsBetweenSpawn = 1;
-    public float distance = 100;
+    public float Distance = 30;
+    public float MaxY = 15;
+
+    public float MinSpeed = 1;
+    public float MaxSpeed = 5;
+    public float RareMinSpeed = 5;
+    public float RareMaxSpeed = 10;
 
     private float secondsSinceLastSpawn = 0;
 
@@ -42,18 +49,18 @@ public class BlockSpawner : MonoBehaviour
         var xPos = Random.Range(MinSpawnX, MaxSpawnX);
         var zPos = Random.Range(MinSpawnZ, MaxSpawnZ);
 
-        var instance = Instantiate(prefab, new Vector3(xPos, -distance, zPos), Quaternion.identity);
+        var instance = Instantiate(prefab, new Vector3(xPos, -Distance, zPos), Quaternion.identity);
         var force = new Vector3(0, 1, 0);
         var destroyOnOutside = instance.gameObject.AddComponent<DestroyOnOutside>();
         destroyOnOutside.Axis = Axis.y;
         destroyOnOutside.Comparison = Comparison.greaterThan;
-        destroyOnOutside.Value = 15;
-        var body = instance.gameObject.AddComponent<Rigidbody>();
+        destroyOnOutside.Value = MaxY;
+        var body = instance.gameObject.GetComponent<Rigidbody>();
         body.useGravity = false;
-        body.AddForce(force * Random.Range(8, 10), ForceMode.Impulse);
+        body.AddForce(force * Random.Range(MinSpeed, MaxSpeed), ForceMode.Impulse);
         if (Random.Range(0, 10) < 1)
         {
-            body.AddForce(force * Random.Range(20, 50), ForceMode.Impulse);            
+            body.AddForce(force * Random.Range(RareMinSpeed, RareMaxSpeed), ForceMode.Impulse);            
         }
         GetComponent<Rigidbody>().AddTorque(Random.insideUnitSphere * Random.Range(1, 20));
     }
